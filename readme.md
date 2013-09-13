af0a2cea304c44ae07d8550af5077395af135138
Joel Vardy <info@joelvardy.com>
2013-09-13 20:45:43 +0100

    Define config and generate user fingerprint

--- readme.md
+++ readme.md
@@ -2,6 +2,25 @@
 
 An authentication library which features access control lists and requires minimal setup.
 
+## Usage
+
+### Configuration
+
+The table below details the available configuration options:
+
+| Option          | Description
+| --------------- | -----
+| secret_key      | Used to add entropy within user fingerprint.
+
+Define these options as shown below:
+
+```php
+// Define authentication details
+Joelvardy\Config::value('authentication', (object) array(
+	'secret_key' => '|X^9VoS4CO7!J6Q@Gg7zE9@hl[q+ki'
+));
+```
+
 Developed by [Joel Vardy][joelvardy].
 
   [joelvardy]: https://joelvardy.com/
--- src/Joelvardy/Authentication.php
+++ src/Joelvardy/Authentication.php
@@ -11,6 +11,54 @@ namespace Joelvardy;
 class Authentication {
 
 
+	protected $config;
+
+
+	/**
+	 * Return current users IP address
+	 *
+	 * @return	void
+	 */
+	public function __construct() {
+
+		// Set configuration
+		$this->config = Config::value('authentication');
+
+		// Ensure a secret key has been set
+		if ( ! isset($this->config->secret_key)) throw new \Exception('You must define a secret key.');
+
+	}
+
+
+	/**
+	 * Return current users IP address
+	 *
+	 * @return	string
+	 */
+	public function ip() {
+
+		// NOTE: This may return the IP address of a proxy server
+
+		return (string) $_SERVER['REMOTE_ADDR'];
+
+	}
+
+
+	/**
+	 * Return user fingerprint
+	 *
+	 * @return	string
+	 */
+	protected function fingerprint() {
+
+		// Initialise the fingerprint with a static salt
+		$fingerprint = md5($config->secret_key);
+		$fingerprint .= $_SERVER['HTTP_USER_AGENT'];
+		$fingerprint .= $this->ip();
+
+		return sha1($fingerprint);
+
+	}
 
 
 }
