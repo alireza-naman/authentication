ae82f66da659cba19f8bf4bb8f6d8daac05dcc45
Joel Vardy <info@joelvardy.com>
2013-09-13 17:52:55 +0100

    Start developing authentication library

---
+++ src/Joelvardy/Authentication.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Joelvardy;
+
+/**
+ * Authentication library
+ *
+ * @link	https://github.com/joelvardy/authentication
+ * @author	Joel Vardy <info@joelvardy.com>
+ */
+class Authentication {
+
+
+
+
+}
